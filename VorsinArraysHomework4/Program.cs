4415828f1812e90a7672f6c72bc7e4e28571dba9
voicefromthesky <andrewdoverin@gmail.com>
2021-10-11 12:49:53 +0300

    first commit

---
+++ VorsinArraysHomework4/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace VorsinArraysHomework4
+{
+    class Program
+    {
+        static int[] Reverse(int[] array)
+        {
+            
+            int length = array.Length;
+            var newArray = new int[length];
+            array.CopyTo(newArray, 0);
+
+            int length1 = array.Length - 1;
+
+            for (int i = 0; i < length; i++)
+            {
+                newArray[i] = array[length1];
+                length1--;
+            }
+            return newArray;
+        }
+
+        static void Main(string[] args)
+        {
+            /*
+            int[] array = { 1, 2, 3, 4, 5, 6,7 };
+            int length = array.Length;
+            var newArray = new int[length];
+            array.CopyTo(newArray, 0);
+            
+            int length1 = array.Length -1;
+
+            for (int i = 0; i < length; i++)
+            {
+                newArray[i] = array[length1];
+                length1--;
+            }
+            
+            
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                Console.WriteLine(newArray[i]);
+            }
+            */
+
+            int[] testArray = { 1, 2, 3, 4, 5, 6, 7 ,8,9,10};
+
+            
+
+            for (int i = 0; i < testArray.Length; i++)
+            {
+                Console.WriteLine(Reverse(testArray)[i]);
+            }
+
+            
+        }
+
+
+    }
+}
